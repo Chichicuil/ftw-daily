23e0daccbc38abb7370a3d487e0efd0e04b06cab
Fer <fer.rc1605@gmail.com>
2020-09-30 20:16:38 -0500

    Changed header to right, adjusted fonts , created a padding constant

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -5,31 +5,32 @@ import classNames from 'classnames';
 import { NamedLink } from '../../components';
 
 import css from './SectionHero.css';
+import { right } from 'inquirer/lib/utils/readline';
 
 const SectionHero = props => {
   const { rootClassName, className } = props;
 
   const classes = classNames(rootClassName || css.root, className);
+  const StylePadding = {
+    padding: "15px",
+  }; 
 
   return (
     <div className={classes}>
       <div className={css.heroContent}>
-        <h1 className={css.heroMainTitle}>
-          <FormattedMessage id="SectionHero.title" />
+        <h1 className={css.heroMainSloganTitle}>
+          &quot;<FormattedMessage id="SectionHero.title" />&quot;
         </h1>
-        <h1 className={css.heroMainTitle}>
-          mejor <span className={css.heroMainTitle2}>amigo</span>
-        </h1>
-        <h2 className={css.heroSubTitle}>
+        <h5 className={css.heroSubTitle}>
           <FormattedMessage id="SectionHero.subTitle" />
-        </h2>
+        </h5>
         <NamedLink
           name="SearchPage"
           to={{
             search:
               'address=Finland&bounds=70.0922932%2C31.5870999%2C59.693623%2C20.456500199999937',
           }}
-          className={css.heroButton}
+          className={css.heroButtonCustom} style= {StylePadding}
         >
           <FormattedMessage id="SectionHero.browseButton" />
         </NamedLink>
